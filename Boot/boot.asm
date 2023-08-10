ce5e6d9e961be265bd50d7dccf3a9d17fcba5e5b
IsaacNewTom <tomgefen22@gmail.com>
2023-08-10 21:29:32 +0300

    Logical block addressing :)

--- Boot/boot.asm
+++ Boot/boot.asm
@@ -1,18 +1,31 @@
+;Directives
+;16 bit mode for real mode and org memory address at 0x7c00 (BIOS loads the boot code from this address)
+;also meaning the stack space would be between 0 and 0x7bfe
 [BITS 16]
 [ORG 0x7c00]
-; The boot code starts at 0x7c00 - the stack would be between 0 and 0x7bfe
 
 start:
-    ; Zero out the registers and set up the stack
+    ;zero out the registers and set up the stack
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00 ; move SP to ORG
-    STI; # set the interrupt flag
+    mov sp, 0x7c00 ;move SP to ORG
+    STI; #set the interrupt flag
+
+TestDiskExtension:
+    ;make sure CPU supports logical block addressing 
+    ;INT 13h AH=41h: Check Extensions Present
+    mov [DriveID], dl ;dl holds the drive ID  when the BIOS transfers control to the boot code
+    mov ah, 0x41
+    mov bx, 0x55aa
+    int 0x13
+    jc LbaNotSupported
+    cmp bx, 0xaa55 ;the carry flag will be set if Extensions are not supported
+    jne LbaNotSupported
 
 PrintMessage:
-    mov ah, 0x13 ;int 0x10, 13
+    mov ah, 0x13 ;int 0x10, type 13
     mov al, 1
     mov bx, 0x17
     xor dx, dx
@@ -24,8 +37,26 @@ End:
     hlt
     jmp End
 
-Message:    db "Hello, welcome to PotongOS!"
+
+;Errors
+LbaNotSupported:
+    ;INT 10h / AH = 13h - write string
+    mov ah, 0x13
+    mov al, 1 ;cursor placement
+    mov bx, 0xc ;light red
+    mov cx, LbaNotSupportedLen
+    xor dx, dx ;row, col
+    mov bp, LbaNotSupportedMsg ;address of the string
+    int 0x10
+    jmp End
+
+
+;Variables
+Message:    db "Hello, welcome to PotongOS! :)"
 MessageLen: equ $-Message
+DriveID: db 0
+LbaNotSupportedMsg: db "Disk Extension error...    "
+LbaNotSupportedLen: equ $-LbaNotSupportedMsg
 
 ;fill the memory between the end of the message up 0x1be with zeroes
 times (0x1be - ($-$$)) db 0
@@ -34,7 +65,7 @@ times (0x1be - ($-$$)) db 0
     db 0, 2, 0 ;starting CHS
     db 0f0h ;the partition type
     db 0ffh, 0ffh, 0ffh ;ending CHS
-    dd 1 ;LBA address of the next sector
+    dd 1 ;logical block addr of the next sector
     dd (20*16*63-1) ;size (10mb, only for the BIOS)
 
     ;zero out the other entries
