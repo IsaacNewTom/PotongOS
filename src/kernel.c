d283c225ef6dec32daab4e61bb99656629219cd4
Tom <tomgefen22@gmail.com>
2024-04-05 15:54:53 +0300

    finished init_terminal and printing char

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,24 @@
 #include "kernel.h"
 
+uint16_t* video_memory = 0;
+
+void init_terminal()
+{
+    int x, y;
+    video_memory = (uint16_t*)(LINEAR_TEXT_BUFF_ADDR);
+    
+    for (y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (x = 0; x < VGA_WIDTH; x++)
+        {
+            video_memory[(y * VGA_WIDTH) + x] = TERMINAL_WRITE_CHAR(' ', 0);
+        }
+    }
+}
+
 void kernel_main()
 {
-
+    init_terminal();
+    video_memory[0] = TERMINAL_WRITE_CHAR('B', YELLOW);
+    
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,5 +1,34 @@
 #ifndef KERNEL_H
 #define KERNEL_H
+#include <stdint.h>
+
+#define LINEAR_TEXT_BUFF_ADDR 0xB8000
+
+/* returns the the value in little endian we need to write to the text buffer to print the given char with the given color */
+#define TERMINAL_WRITE_CHAR(chr, color) ((color << 8) | chr)
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+
+enum COLOR{
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    PURPLE,
+    BROWN,
+    GRAY,
+    DARK_GRAY,
+    LIGHT_BLUE,
+    LIGHT_GREEN,
+    LIGHT_CYAN,
+    LIGHT_RED,
+    LIGHT_PURPLE,
+    YELLOW,
+    WHITE
+};
+
 
 void kernel_main();
 
