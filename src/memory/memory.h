94215c09b0af2140dcb8c6e356f8eeddd6116802
Tom <tomgefen22@gmail.com>
2024-04-06 21:49:53 +0300

    removed comment from header file

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -2,7 +2,6 @@
 #define MEMORY_H
 #include <stddef.h>
 
-/* fills the first *size* bytes of the memory area pointed to by ptr with the constant byte c */
 void* memset(void* ptr, int c, size_t size);
 
 #endif
