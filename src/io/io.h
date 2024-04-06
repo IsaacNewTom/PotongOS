357db5441820fce52c41aa3fbc60372f8e438f8a
Tom <tomgefen22@gmail.com>
2024-04-06 21:48:25 +0300

    added comments

--- src/io/io.h
+++ src/io/io.h
@@ -1,10 +1,14 @@
 #ifndef IO_H
 #define IO_H
-
+/* see io.asm for the source */
+/* reads a byte from the given port*/
 unsigned char insb(unsigned short port);
+/* reads a word from the given port */
 unsigned short insw(unsigned short port);
 
+/* writes the value (byte) to the given port */
 void outb(unsigned short port, unsigned char value);
+/* writes the value (word) to the given port */
 void outw(unsigned short port, unsigned short value);
 
 #endif
--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "memory.h"
 
+/* fills the first *size* bytes of the memory area pointed to by ptr with the constant byte c */
 void* memset(void* ptr, int c, size_t size)
 {
     char* helper_ptr = (char*) ptr;
